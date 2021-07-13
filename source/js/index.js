58eae67991d68ee07973c671aee6906473cad56c
FALVO <anthony.falvo.pro@gmail.com>
2021-07-13 07:34:00 +0200

    js en cours

---
+++ source/js/index.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+
+    navigator.serviceWorker
+        .register('/serviceWorker.js')
+        .then(() => console.log('sw registered'))
+        .catch((err) => console.log(err));
+
+};
